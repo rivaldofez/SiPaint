4dfd5b33b4a1a78e04dd99b18e34b95e863a7542
rivaldofez <rivaldofez@gmail.com>
2022-04-15 00:31:24 +0700

    add canvas and function drawing line

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,36 @@
 import SwiftUI
 
+
+struct Line {
+    var points = [CGPoint]()
+    var color: Color = .red
+    var lineWidth: Double = 1.0
+}
+
+
 struct ContentView: View {
+    
+    @State private var currentLine = Line()
+    @State private var lines: [Line] = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            Canvas { context, size in
+                for line in lines {
+                    var path = Path()
+                    path.addLines(line.points)
+                    context.stroke(path, with: .color(line.color), lineWidth: line.lineWidth)
+                }
+            }.gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                .onChanged({value in
+                    let newPoint = value.location
+                    currentLine.points.append(newPoint)
+                    self.lines.append(currentLine)
+                })
+                    .onEnded({Value in
+                        
+                    })
+            )
+        }.frame(minWidth: 400, minHeight: 400)
     }
 }
